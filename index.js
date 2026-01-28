0d2953d0e312ae26329bc8213c79efd42f421cfd
mileszh <mileszh05@gmail.com>
2026-01-27 22:30:41 -0500

    wakeup-01: Add HTML structure with button, wakeup-02: Add sleep cycle calculation

---
+++ index.js
@@ -0,0 +1,19 @@
+function calcWakeUpTimes() {
+  const fallAsleepTime = new Date();
+  fallAsleepTime.setMinutes(fallAsleepTime.getMinutes() + 14);
+
+  const wakeUpTime = new Date(fallAsleepTime);
+  const wakeUpTimes = [];
+
+  for (let i = 1; i <= 6; i++) {
+    wakeUpTime.setMinutes(wakeUpTime.getMinutes() + 90);
+    const timeString = wakeUpTime.toLocaleTimeString("en-US", {
+      timeStyle: "short",
+    });
+    wakeUpTimes.push(timeString);
+  }
+
+  console.log(wakeUpTimes);
+}
+
+calcWakeUpTimes();
