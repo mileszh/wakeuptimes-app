efb3530772e00c877b4a742092d85d2aa5f0ad49
mileszh <mileszh05@gmail.com>
2026-01-27 22:39:32 -0500

    wakeup-03: Add DOM manipulation

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
+const calcBtn = document.getElementById("calc-btn");
+calcBtn.addEventListener("click", calcWakeUpTimes);
+const wakeUpHoursDiv = document.getElementById("wakeup-hours-div");
+
 function calcWakeUpTimes() {
   const fallAsleepTime = new Date();
   fallAsleepTime.setMinutes(fallAsleepTime.getMinutes() + 14);
 
   const wakeUpTime = new Date(fallAsleepTime);
-  const wakeUpTimes = [];
+  wakeUpHoursDiv.innerHTML = "";
 
   for (let i = 1; i <= 6; i++) {
     wakeUpTime.setMinutes(wakeUpTime.getMinutes() + 90);
     const timeString = wakeUpTime.toLocaleTimeString("en-US", {
       timeStyle: "short",
     });
-    wakeUpTimes.push(timeString);
+
+    const cycleDiv = document.createElement("div");
+    cycleDiv.setAttribute("id", `cycle-${i}`);
+    cycleDiv.textContent = timeString;
+    wakeUpHoursDiv.appendChild(cycleDiv);
   }
-
-  console.log(wakeUpTimes);
 }
-
-calcWakeUpTimes();
